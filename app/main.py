0a71df8df177d90e816f4ace56904aebc56d0100
Joe <joeczepil@gmail.com>
2018-03-03 11:43:53 -0800

    running snek?

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 import bottle
 import os
 import random
-
-
+from pandas import *
 
 @bottle.route('/')
 def static():
@@ -50,88 +49,228 @@ def compute_food_score(score_x, score_y, x, y):
     }
     distance = compute_distance(block1, block2)
 
-    return 6 - distance
+    return 20 - distance
+
+def compute_bad_score(score_x, score_y, x, y):
+    block1 = {
+        'x': score_x,
+        'y': score_y
+    }
+    block2 = {
+        'x': x,
+        'y': y
+    }
+    distance = compute_distance(block1, block2)
+
+    if distance == 2:
+        return 10
+    elif distance == 1:
+        return 50
+    else:
+        return 900
 
 def add_food_points(food, board):
     x = food['x']
     y = food['y']
     for horiz in range(-2, 3):
         for vert in range(-2, 3):
-            board[x+horiz][y+vert] += compute_food_score(horiz, vert, x, y)
+            if 0 <= x+horiz < len(board[0]) and 0 <= y+vert < len(board[0]):
+                board[x+horiz][y+vert] += compute_food_score(horiz, vert, x, y)
 
     return board
 
 def plan_attack(scary, neck, head, board):
+    board[head['x']][head['y']] -= 900
+    x = head['x']
+    y = head['y']
     if scary:
-        board[head['x']+1][head['y']] -= 999
-        board[head['x']-1][head['y']] -= 999
-        board[head['x']][head['y']+1] -= 999
-        board[head['x']][head['y']-1] -= 999
+        for horiz in range(-1, 2):
+            for vert in range(-1, 2):
+                if 0 <= x+horiz < len(board[0]) and 0 <= y+vert < len(board[0]):
+                    board[x+horiz][y+vert] -= compute_bad_score(horiz, vert, x, y)
     else:
-        if neck['x'] != head['x'] and neck['y'] != head['y']:
-            board[head['x']+1][head['y']] += 500
-        if neck['x'] != head['x'] and neck['y'] != head['y']:
-            board[head['x']-1][head['y']] += 500
-        if neck['x'] != head['x'] and neck['y'] != head['y']:
-            board[head['x']][head['y']+1] += 500
-        if neck['x'] != head['x'] and neck['y'] != head['y']:
-            board[head['x']][head['y']-1] += 500
+        if neck['x'] != x and neck['y'] != y:
+            if 0 <= x+1 < len(board):
+                board[x+1][y] += 500
+        if neck['x'] != x and neck['y'] != y:
+            if 0 <= x-1 < len(board):
+                board[x-1][y] += 500
+        if neck['x'] != x and neck['y'] != y:
+            if 0 <= y+1 < len(board):
+                board[x][y+1] += 500
+        if neck['x'] != x and neck['y'] != y:
+            if 0 <= y-1 < len(board):
+                board[x][y-1] += 500
 
     return board
 
 def plan_survival(body_part, board):
+    board[body_part['x']][body_part['y']] -= 900
     return board
 
 def dont_kill_yourself(myself, board):
+    board[myself['x']][myself['y']] -= 900
     return board
 
-
-@bottle.post('/move')
-def move():
-    data = bottle.request.json
-
+def get_move(data):
     board = [[0]*data['height'] for _ in range(data['width'])]
     my_head = data['you']['body']['data'][0]
+    my_id = data['you']['id']
 
     for food in data['food']['data']:
-        if compute_distance(my_head, food) < 5:
+        if compute_distance(my_head, food) < 20:
             board = add_food_points(food, board)
 
     for snake in data['snakes']['data']:
         snake_part = 0
-        for body_part, index in enum(snake['body']['data']):
-            if compute_distance(food, body_part) < 5:
-                if(snake_part == 0):
-                    if(len(snake['body']['data']) <= len(data['you']['body']['data'])):
-                        board = plan_attack(True, snake['body']['data'][index+1], body_part, board)
+        print('snake found')
+        if(not (my_id == snake['id'] or snake['health'] == 0)):
+            for index, body_part in enumerate(snake['body']['data']):
+                if compute_distance(food, body_part) < 40:
+                    if(snake_part == 0):
+                        if(len(snake['body']['data']) <= len(data['you']['body']['data'])):
+                            print('scary snek!')
+                            board = plan_attack(True, snake['body']['data'][index+1], body_part, board)
+                        else:
+                            board = plan_attack(False, neck, body_part, board)
                     else:
-                        board = plan_attack(False, neck, body_part, board)
-                else:
-                    board = plan_survival(body_part, board)
-                snake_part += 1
+                        print('placing a -900')
+                        board = plan_survival(body_part, board)
+                    snake_part += 1
+
+    print DataFrame(board)
 
     for body_part in data['you']['body']['data']:
         board = dont_kill_yourself(body_part, board)
 
-    options = {board[my_head['x']][my_head['y']], board[my_head['x']][my_head['y']], board[my_head['x']][my_head['y']], board[my_head['x']][my_head['y']]}
+    options = dict([('left', board[my_head['x']][my_head['y']-1]), ('up', board[my_head['x']+1][my_head['y']]), ('down', board[my_head['x']-1][my_head['y']]), ('right', board[my_head['x']][my_head['y']+1])])
 
-    direction = min(options)
-
-    directions = ['up', 'down', 'left', 'right']
-    direction = random.choice(directions)
+    direction = min(options, key=options.get)
     print direction
     return {
         'move': direction,
         'taunt': 'Snakes Everywhere! Ahhh!'
     }
 
+@bottle.post('/move')
+def move():
+    data = bottle.request.json
 
 # Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app()
 
 if __name__ == '__main__':
-    bottle.run(
-        application,
-        host=os.getenv('IP', '0.0.0.0'),
-        port=os.getenv('PORT', '8080'),
-        debug = True)
+    data = {
+      "food": {
+        "data": [
+          {
+            "object": "point",
+            "x": 0,
+            "y": 9
+          }
+        ],
+        "object": "list"
+      },
+      "height": 20,
+      "id": 1,
+      "object": "world",
+      "snakes": {
+        "data": [
+          {
+            "body": {
+              "data": [
+                {
+                  "object": "point",
+                  "x": 13,
+                  "y": 19
+                },
+                {
+                  "object": "point",
+                  "x": 13,
+                  "y": 18
+                },
+                {
+                  "object": "point",
+                  "x": 13,
+                  "y": 17
+                }
+              ],
+              "object": "list"
+            },
+            "health": 100,
+            "id": "58a0142f-4cd7-4d35-9b17-815ec8ff8e70",
+            "length": 3,
+            "name": "Sonic Snake",
+            "object": "snake",
+            "taunt": "Gotta go fast"
+          },
+          {
+            "body": {
+              "data": [
+                {
+                  "object": "point",
+                  "x": 8,
+                  "y": 15
+                },
+                {
+                  "object": "point",
+                  "x": 7,
+                  "y": 15
+                },
+                {
+                  "object": "point",
+                  "x": 6,
+                  "y": 15
+                }
+              ],
+              "object": "list"
+            },
+            "health": 100,
+            "id": "48ca23a2-dde8-4d0f-b03a-61cc9780427e",
+            "length": 3,
+            "name": "Typescript Snake",
+            "object": "snake",
+            "taunt": ""
+          }
+        ],
+        "object": "list"
+      },
+      "turn": 0,
+      "width": 20,
+      "you": {
+        "body": {
+          "data": [
+            {
+              "object": "point",
+              "x": 8,
+              "y": 15
+            },
+            {
+              "object": "point",
+              "x": 8,
+              "y": 14
+            },
+            {
+              "object": "point",
+              "x": 7,
+              "y": 14
+            }
+          ],
+          "object": "list"
+        },
+        "health": 100,
+        "id": "48ca23a2-dde8-4d0f-b03a-61cc9780427e",
+        "length": 3,
+        "name": "Typescript Snake",
+        "object": "snake",
+        "taunt": ""
+      }
+    }
+
+    get_move(data)
+
+    # bottle.run(
+    #     application,
+    #     host=os.getenv('IP', '0.0.0.0'),
+    #     port=os.getenv('PORT', '8080'),
+    #     debug = True)
